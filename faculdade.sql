849be74269f652bea80e610d0dd719fa8ced54bc
LCZW973 <leonardoczanin@gmail.com>
2026-05-08 19:55:50 -0300

    Update faculdade.sql

--- faculdade.sql
+++ faculdade.sql
@@ -3,102 +3,113 @@ CREATE DATABASE faculdade;
 USE faculdade;
 
 CREATE TABLE supervisor(
-	cpf LONG PRIMARY KEY,
-    nome TEXT NOT NULL,
+    cpf BIGINT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
     email VARCHAR(30)
 );
 
 CREATE TABLE sala(
-	numero INT PRIMARY KEY,
+    numero INT PRIMARY KEY,
     vagas INT,
-    cpf_supervisor INT,
-    
-    CONSTRAINT fk1
-    FOREIGN KEY (cpf_supervisor) REFERENCES supervisor(cpf) -- Arrumar depois
+    cpf_supervisor BIGINT,
+
+    CONSTRAINT fk_sala_supervisor
+    FOREIGN KEY (cpf_supervisor)
+    REFERENCES supervisor(cpf)
 );
 
 CREATE TABLE vestibulando(
-	cpf LONG PRIMARY KEY,
-    nome TEXT NOT NULL,
+    cpf BIGINT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
     email VARCHAR(30),
     numero_sala INT,
-    
-    CONSTRAINT fk1
-    FOREIGN KEY (numero_sala) REFERENCES sala(numero) -- Ver depois
+
+    CONSTRAINT fk_vestibulando_sala
+    FOREIGN KEY (numero_sala)
+    REFERENCES sala(numero)
 );
 
 CREATE TABLE vestibular(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	localizacao TEXT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    localizacao VARCHAR(100) NOT NULL,
     data_realizacao DATE NOT NULL
 );
 
 CREATE TABLE materia(
-	id_vestibular INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_vestibular INT,
     materia VARCHAR(15) NOT NULL,
-    
-    CONSTRAINT fk1
-    FOREIGN KEY (id_vestibular) REFERENCES vestibular(id) -- Ver depois
+
+    CONSTRAINT fk_materia_vestibular
+    FOREIGN KEY (id_vestibular)
+    REFERENCES vestibular(id)
 );
 
 CREATE TABLE vestibulando_presta_vestibular(
-	id_vestibular INT,
-    cpf_vestibulando LONG,
+    id_vestibular INT,
+    cpf_vestibulando BIGINT,
     numero_inscricao INT,
-    
-    CONSTRAINT fk1
-    FOREIGN KEY (id_vestibular) REFERENCES vestibular(id) -- Arrumar depois
-    , 
-    CONSTRAINT fk2 
-    FOREIGN KEY (cpf_vestibulando) REFERENCES vestibulando(cpf) -- Arrumar depois
-    ,
-    PRIMARY KEY(id_vestibular, cpf_vestibulando)
+
+    PRIMARY KEY(id_vestibular, cpf_vestibulando),
+
+    CONSTRAINT fk_vpv_vestibular
+    FOREIGN KEY (id_vestibular)
+    REFERENCES vestibular(id),
+
+    CONSTRAINT fk_vpv_vestibulando
+    FOREIGN KEY (cpf_vestibulando)
+    REFERENCES vestibulando(cpf)
 );
 
 CREATE TABLE curso(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    nome TEXT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100)
 );
 
 CREATE TABLE curso_oferece_vestibular(
-	id_vestibular INT,
+    id_vestibular INT,
     id_curso INT,
     vagas INT NOT NULL,
-    
-    CONSTRAINT fk1
-    FOREIGN KEY (id_vestibular) REFERENCES vestibular(id) -- Arrumar depois
-    , 
-    CONSTRAINT
-    FOREIGN KEY (id_curso) REFERENCES curso(id) -- Arrumar depois
-    ,
-    PRIMARY KEY(id_vestibular, id_curso)
-);
 
-CREATE TABLE prova(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    edicao INT NOT NULL,
-    cpf_corretor LONG,
-    
-    CONSTRAINT
-    FOREIGN KEY (cpf_corretor) REFERENCES corretor(cpf) -- Arrumar depois
-);
+    PRIMARY KEY(id_vestibular, id_curso),
 
-CREATE TABLE vestibulando_realiza_prova(
-	cpf_vestibulando LONG,
-    id_prova INT,
-    nota FLOAT,
-    
-    CONSTRAINT
-    FOREIGN KEY (cpf_vestibulando) REFERENCES vestibulando(cpf) -- Arrumar depois
-    ,
-    CONSTRAINT
-    FOREIGN KEY (id_prova) REFERENCES prova(id) -- Arrumar depois
-    ,
-    PRIMARY KEY(cpf_vestibulando, id_prova)
+    CONSTRAINT fk_cov_vestibular
+    FOREIGN KEY (id_vestibular)
+    REFERENCES vestibular(id),
+
+    CONSTRAINT fk_cov_curso
+    FOREIGN KEY (id_curso)
+    REFERENCES curso(id)
 );
 
 CREATE TABLE corretor(
-	cpf LONG PRIMARY KEY,
-    nome TEXT NOT NULL,
+    cpf BIGINT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
     email VARCHAR(30)
+);
+
+CREATE TABLE prova(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    edicao INT NOT NULL,
+    cpf_corretor BIGINT,
+
+    CONSTRAINT fk_prova_corretor
+    FOREIGN KEY (cpf_corretor)
+    REFERENCES corretor(cpf)
+);
+
+CREATE TABLE vestibulando_realiza_prova(
+    cpf_vestibulando BIGINT,
+    id_prova INT,
+    nota FLOAT,
+
+    PRIMARY KEY(cpf_vestibulando, id_prova),
+
+    CONSTRAINT fk_vrp_vestibulando
+    FOREIGN KEY (cpf_vestibulando)
+    REFERENCES vestibulando(cpf),
+
+    CONSTRAINT fk_vrp_prova
+    FOREIGN KEY (id_prova)
+    REFERENCES prova(id)
 );
