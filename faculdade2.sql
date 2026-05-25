149b2710690e56a8e0e61bed445844f987985931
LCZW973 <leonardoczanin@gmail.com>
2026-05-25 17:50:18 -0300

    Update faculdade2.sql

--- faculdade2.sql
+++ faculdade2.sql
@@ -2,119 +2,213 @@ DROP DATABASE IF EXISTS faculdade;
 CREATE DATABASE faculdade;
 USE faculdade;
 
+-- CRIAR 2 USARUIS ARBITRARIOS--
+DROP USER IF EXISTS 'Elton'@'%';
+DROP USER IF EXISTS 'Elias'@'%';
+ 
+CREATE USER 'Elton'@'%' IDENTIFIED BY '00o00';
+CREATE USER 'Elias'@'%' IDENTIFIED BY '00i00';
+ 
+-- CRIAR1 ROLE E GARANTIR NO MINIMO 2 PRIVILEGIOD E INSERIR USARRIOS NELA POR PADRAO --
+DROP ROLE IF EXISTS 'Insert_select';
+ 
+CREATE ROLE 'Insert_select';
+GRANT INSERT,SELECT ON faculdade.* TO 'Insert_select';
+GRANT  'Insert_select' TO 'Elton'@'%';
+GRANT  'Insert_select' TO 'Elias'@'%';
+SET DEFAULT ROLE  'Insert_select'  TO 'Elton'@'%';
+SET DEFAULT ROLE  'Insert_select'  TO 'Elias'@'%';
+ 
+ 
+-- FUNCAO RETORNA A VAGA DA SALA SELECIONADA--
+DELIMITER $$
+DROP FUNCTION IF EXISTS retornavagas $$
+CREATE FUNCTION retornavagas (id_sala INT ) 
+RETURNS INT 
+DETERMINISTIC
+BEGIN
+ 
+    DECLARE vagasTotais INT;
+    SELECT vagas
+    INTO vagasTotais
+    FROM sala WHERE numero = id_sala ;
+    RETURN vagasTotais;
+END$$
+DELIMITER ;
+ 
+-- FUNCAO QUE CALCULA O NUMERO DE VESTIBULANDO ATUAIS NA SALA SELECIONADA --
+DELIMITER $$
+DROP FUNCTION IF EXISTS contavestibulandos $$
+CREATE FUNCTION contavestibulandos ( id_sala INT) 
+RETURNS INT 
+DETERMINISTIC
+BEGIN
+ 
+    DECLARE numerovestibulandos INT;
+ 
+    SELECT COUNT(cpf)
+    INTO numerovestibulandos
+    FROM vestibulando  WHERE vestibulando.numero_sala = id_sala;
+ 
+    RETURN numerovestibulandos;
+ 
+END$$
+DELIMITER ;
+ 
+-- TRIGGERS QUE USA AS FUNCOES ANTERIORES PARA CONTROLAR O NUMERO DE VESTIBULANDOS EM SALA DE ACORDO COM AS VAGAS POR SALA--
+DELIMITER $$
+CREATE TRIGGER controlavagas
+BEFORE INSERT ON vestibulando
+FOR EACH ROW
+BEGIN
+ 
+    DECLARE vagasTotais INT;
+    DECLARE vestibulandos INT;
+ 
+    SET vagasTotais = retornavagas(NEW.numero_sala);
+    SET vestibulandos = contavestibulandos(NEW.numero_sala);
+ 
+    IF vestibulandos >= vagasTotais THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Sem vagas disponiveis na sala ';
+    END IF;
+ 
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER controlavagasupdate
+BEFORE UPDATE ON vestibulando
+FOR EACH ROW
+BEGIN
+ 
+    DECLARE vagasTotais INT;
+    DECLARE vestibulandos INT;
+ 
+    SET vagasTotais = retornavagas(NEW.numero_sala);
+    SET vestibulandos = contavestibulandos(NEW.numero_sala);
+ 
+    IF vestibulandos >= vagasTotais THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Sem vagas disponiveis na sala ';
+    END IF;
+ 
+END$$
+DELIMITER ;
+ 
 -- CRIAR NO MINIMO 5 TABELAS E RELACIONAMENTOS COESES ENTRE ELAS --
 CREATE TABLE supervisor(
     cpf BIGINT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(30)
 );
-
+ 
 CREATE TABLE sala(
     numero INT PRIMARY KEY,
     vagas INT,
     cpf_supervisor BIGINT,
-
+ 
     CONSTRAINT fk_sala_supervisor
     FOREIGN KEY (cpf_supervisor)
     REFERENCES supervisor(cpf)
 );
-
+ 
 CREATE TABLE vestibulando(
     cpf BIGINT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(30),
     numero_sala INT,
-
+ 
     CONSTRAINT fk_vestibulando_sala
     FOREIGN KEY (numero_sala)
     REFERENCES sala(numero)
 );
-
+ 
 CREATE TABLE vestibular(
     id INT AUTO_INCREMENT PRIMARY KEY,
     localizacao VARCHAR(100) NOT NULL,
     data_realizacao DATE NOT NULL
 );
-
+ 
 CREATE TABLE materia(
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_vestibular INT,
     materia VARCHAR(15) NOT NULL,
-
+ 
     CONSTRAINT fk_materia_vestibular
     FOREIGN KEY (id_vestibular)
     REFERENCES vestibular(id)
 );
-
+ 
 CREATE TABLE vestibulando_presta_vestibular(
     id_vestibular INT,
     cpf_vestibulando BIGINT,
     numero_inscricao INT,
-
+ 
     PRIMARY KEY(id_vestibular, cpf_vestibulando),
-
+ 
     CONSTRAINT fk_vpv_vestibular
     FOREIGN KEY (id_vestibular)
     REFERENCES vestibular(id),
-
+ 
     CONSTRAINT fk_vpv_vestibulando
     FOREIGN KEY (cpf_vestibulando)
     REFERENCES vestibulando(cpf)
 );
-
+ 
 CREATE TABLE curso(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100)
 );
-
+ 
 CREATE TABLE curso_oferece_vestibular(
     id_vestibular INT,
     id_curso INT,
     vagas INT NOT NULL,
-
+ 
     PRIMARY KEY(id_vestibular, id_curso),
-
+ 
     CONSTRAINT fk_cov_vestibular
     FOREIGN KEY (id_vestibular)
     REFERENCES vestibular(id),
-
+ 
     CONSTRAINT fk_cov_curso
     FOREIGN KEY (id_curso)
     REFERENCES curso(id)
 );
-
+ 
 CREATE TABLE corretor(
     cpf BIGINT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(30)
 );
-
+ 
 CREATE TABLE prova(
     id INT AUTO_INCREMENT PRIMARY KEY,
     edicao INT NOT NULL,
     cpf_corretor BIGINT,
-
+ 
     CONSTRAINT fk_prova_corretor
     FOREIGN KEY (cpf_corretor)
     REFERENCES corretor(cpf)
 );
-
+ 
 CREATE TABLE vestibulando_realiza_prova(
     cpf_vestibulando BIGINT,
     id_prova INT,
     nota FLOAT,
-
+ 
     PRIMARY KEY(cpf_vestibulando, id_prova),
-
+ 
     CONSTRAINT fk_vrp_vestibulando
     FOREIGN KEY (cpf_vestibulando)
     REFERENCES vestibulando(cpf),
-
+ 
     CONSTRAINT fk_vrp_prova
     FOREIGN KEY (id_prova)
     REFERENCES prova(id)
 );
-
+ 
 -- INSERIR NO MINIMO 5REGISTROS PARA CADA TABELA --
 -- SUPERVISOR--
 INSERT INTO supervisor VALUES
@@ -125,7 +219,7 @@ INSERT INTO supervisor VALUES
 (55555555555,'Joao Rocha','joao@gmail.com');
 -- SALA --
 INSERT INTO sala VALUES
-(101,40,11111111111),
+(101,2,11111111111),
 (102,35,22222222222),
 (103,50,33333333333),
 (104,45,44444444444),
@@ -133,8 +227,8 @@ INSERT INTO sala VALUES
 -- VESTIBULANDO --
 INSERT INTO vestibulando VALUES
 (99911111111,'Lucas Pereira','lucas@gmail.com',101),
-(99922222222,'Mariana Costa','mariana@gmail.com',102),
-(99933333333,'Pedro Henrique','pedro@gmail.com',103),
+(99922222222,'Mariana Costa','mariana@gmail.com',101),
+(99933333333,'Pedro Henrique','pedro@gmail.com',101),
 (99944444444,'Julia Martins','julia@gmail.com',104),
 (99955555555,'Gabriel Souza','gabriel@gmail.com',105);
 -- VESTIBULAR --
@@ -193,76 +287,5 @@ INSERT INTO vestibulando_realiza_prova VALUES
 (99933333333,3,9.2),
 (99944444444,4,6.8),
 (99955555555,5,8.9);
-
--- CRIAR 2 USARUIS ARBITRARIOS--
-CREATE USER 'Elton'@'%' IDENTIFIED BY '00o00';
-CREATE USER 'Elias'@'%' IDENTIFIED BY '00i00';
-
--- CRIAR1 ROLE E GARANTIR NO MINIMO 2 PRIVILEGIOD E INSERIR USARRIOS NELA POR PADRAO --
-CREATE ROLE 'Insert_select';
-GRANT INSERT,SELECT ON faculdade.* TO 'Insert_select';
-GRANT  'Insert_select' TO 'Elton'@'%';
-GRANT  'Insert_select' TO 'Elias'@'%';
-SET DEFAULT ROLE  'Insert_select'  TO 'Elton'@'%';
-SET DEFAULT ROLE  'Insert_select'  TO 'Elias'@'%';
-
-
--- FUNCAO RETORNA A VAGA DA SALA SELECIONADA--
-DELIMITER $$
-DROP FUNCTION IF EXISTS contavagas $$
-CREATE FUNCTION contavagas (id_sala INT ) 
-RETURNS INT 
-DETERMINISTIC
-BEGIN
-
-    DECLARE vagasTotais INT;
-    SELECT vagas
-    INTO vagasTotais
-    FROM sala WHERE numero = id_sala ;
-    
-    RETURN vagasTotais;
-    
-END$$
-DELIMITER ;
-
--- FUNCAO QUE CALCULA O NUMERO DE VESTIBULANDO ATUAIS NA SALA SELECIONADA --
-DELIMITER $$
-DROP FUNCTION IF EXISTS contavestibulandos $$
-CREATE FUNCTION contavestibulandos ( id_sala INT) 
-RETURNS INT 
-DETERMINISTIC
-BEGIN
-
-    DECLARE numerovestibulandos INT;
-
-    SELECT COUNT(cpf)
-    INTO numerovestibulandos
-    FROM vestibulando  WHERE vestibulando. numero_sala = id_sala;
-
-    RETURN numerovestibulandos;
-
-END$$
-DELIMITER ;
-
--- TRIGGER QUE USA AS FUNCOES ANTERIORES PARA CONTROLAR O NUMERO DE VESTIBULANDOS EM SALA DE ACORDO COM AS VAGAS POR SALA--
-DELIMITER $$
-CREATE TRIGGER controlavagas
-BEFORE INSERT ON vestibulando
-FOR EACH ROW
-BEGIN
-
-    DECLARE vagasTotais INT;
-    DECLARE vestibulandos INT;
-
-    SET vagasTotais = contavagas(NEW.numero_sala);
-    SET vestibulandos = contavestibulandos(NEW.numero_sala);
-
-    IF vestibulandos >= vagasTotais THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Sem vagas disponiveis na sala ';
-    END IF;
-
-END$$
-DELIMITER ;
-
+ 
 SELECT * FROM vestibulando;
